f828e4c31252be702d0fb60ca1b17c4bfe8bd648
Padhoab <aniruddh.seth.123@gmail.com>
2026-06-11 17:22:02 +0530

    Create search-data.js

---
+++ search-data.js
@@ -0,0 +1,27 @@
+const searchData = [
+
+{
+title:"Parts of Indian Constitution",
+url:"articles/parts-of-indian-constitution.html",
+keywords:"constitution parts schedules articles"
+},
+
+{
+title:"Landmark Judgments",
+url:"landmark-judgments.html",
+keywords:"kesavananda basic structure supreme court"
+},
+
+{
+title:"Bare Acts",
+url:"bare-acts.html",
+keywords:"bns bnss bsa ipc crpc evidence act"
+},
+
+{
+title:"Current Affairs",
+url:"current-affairs.html",
+keywords:"daily monthly current affairs"
+}
+
+];
