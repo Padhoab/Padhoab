34c18ce27d90741684afa73f62976cfed194ca3b
Padhoab <aniruddh.seth.123@gmail.com>
2026-06-13 18:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const headerContainer = document.getElementById("header");
 
 if (headerContainer) {
-fetch("header.html")
-.then(response => response.text())
-.then(data => {
-headerContainer.innerHTML = data;
-});
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      headerContainer.innerHTML = data;
+    });
 }
 
 const footerContainer = document.getElementById("footer");
 
 if (footerContainer) {
-fetch("footer.html")
-.then(response => response.text())
-.then(data => {
-footerContainer.innerHTML = data;
-});
+  fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+      footerContainer.innerHTML = data;
+    });
 }
