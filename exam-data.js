101d0d269a106226a6cb034c417ecf5e49de056f
Padhoab <aniruddh.seth.123@gmail.com>
2026-06-14 07:38:59 +0530

    Create exam-data.js

---
+++ exam-data.js
@@ -0,0 +1,38 @@
+const examData = {
+
+    "haryana-ada-screening": {
+        name: "Haryana ADA 2026 (Screening Test)",
+        date: new Date("2026-07-05")
+    },
+
+    "bihar-apo": {
+        name: "Bihar APO 2026",
+        date: new Date("2026-07-15")
+    },
+
+    "bpsc-72nd": {
+        name: "BPSC 2026 (72nd CCE)",
+        date: new Date("2026-07-26")
+    },
+
+    "haryana-ada-subject": {
+        name: "Haryana ADA 2026 (Subject Knowledge Test)",
+        date: new Date("2026-08-09")
+    },
+
+    "rajasthan-apo": {
+        name: "Rajasthan APO 2026",
+        date: new Date("2026-09-02")
+    },
+
+    "uppcs-prelims": {
+        name: "UPPCS Prelims 2026",
+        date: new Date("2026-12-06")
+    },
+
+    "upsc-prelims": {
+        name: "UPSC Prelims 2027",
+        date: new Date("2027-05-23")
+    }
+
+};
