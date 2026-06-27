a32bee342b486b3a304b1597b577c084446305df
Padhoab <padhoab.1@gmail.com>
2026-06-27 20:55:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-import { auth,db } from "./firebase.js";
-
-import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut,
-onAuthStateChanged
-}
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
 const headerContainer = document.getElementById("header");
 
-import {
-doc,
-setDoc
-}
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
 if (headerContainer) {
-
   fetch("../header.html")
     .then(response => {
-      if (!response.ok) {
-        return fetch("header.html");
-      }
+      if (!response.ok) return fetch("header.html");
       return response;
     })
     .then(response => response.text())
@@ -34,12 +15,9 @@ if (headerContainer) {
 const footerContainer = document.getElementById("footer");
 
 if (footerContainer) {
-
   fetch("../footer.html")
     .then(response => {
-      if (!response.ok) {
-        return fetch("footer.html");
-      }
+      if (!response.ok) return fetch("footer.html");
       return response;
     })
     .then(response => response.text())
